c2dd298760088e8cd316192e2c615cc32631483b
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2024-07-30 01:06:17 +0530

    category ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/CategoryList"
 import ProductList from "@/components/ProductList"
 import Slider from "@/components/Slider"
 
@@ -7,7 +8,12 @@ const HomePage = () => {
         <Slider />
         <div className="mt-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
           <h1 className="text-3xl font-semibold">Featured Products</h1>
-        <ProductList />
+          <ProductList />
+        </div>
+
+        <div className="mt-12">
+          <h1 className="text-3xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64  font-semibold">Categories</h1>
+          <CategoryList />
         </div>
 
     </div>
